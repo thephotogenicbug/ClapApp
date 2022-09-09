bc423d167c34b7ef3058e1eeae8b7987130f6e3d
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-09 23:45:38 +0530

    Build logics to Play Audio

--- app/src/main/java/com/naveen/clapapp/MainActivity.kt
+++ app/src/main/java/com/naveen/clapapp/MainActivity.kt
@@ -3,10 +3,18 @@ package com.naveen.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer : MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+        val button = findViewById<Button>(R.id.btnClap)
+        button.setOnClickListener {
+            mediaPlayer.start()
+        }
     }
 }
